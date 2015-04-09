bdf1dd8b0115f02879341b349b029f9ba79743f5
andrliu <dieseliu321@gmail.com>
2015-04-08 21:34:11 -0700

    Initial Commit

---
+++ Circle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Circle
+//
+//  Created by Andrew Liu on 4/8/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Circle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Circle
+//
+//  Created by Andrew Liu on 4/8/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
